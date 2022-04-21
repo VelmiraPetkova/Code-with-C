b9076ce1735d0c7658ad8a15fb65f0aa3938bce9
VelmiraPetkova <104137851+VelmiraPetkova@users.noreply.github.com>
2022-04-21 13:16:36 +0300

    Add files via upload

---
+++ favorit_num .c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int Favorite_num;
+    
+    printf("Enter your favorite num: ");
+    scanf("%d", Favorite_num);
+    printf("My is the same");
+    return 0;
+}
